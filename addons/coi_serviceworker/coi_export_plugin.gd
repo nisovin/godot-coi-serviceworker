8eaa76c2aeb6d9ce5a2a1fa290cb1d80128bab70
Justin <nisovin@pm.me>
2023-10-06 20:57:06 -0600

    Add options for popout window
    If a game is embedded in an iframe, it often still won't work. This export option creates an index.html that pops open the game outside of the iframe, which may allow the game to run in many situations

--- addons/coi_serviceworker/coi_export_plugin.gd
+++ addons/coi_serviceworker/coi_export_plugin.gd
@@ -6,15 +6,68 @@ const JS_FILE = "coi-serviceworker.min.js"
 var plugin_path: String = get_script().resource_path.get_base_dir()
 var exporting_web := false
 var export_path := ""
+var update_export_options := true
 
 func _get_name() -> String:
 	return "CoiServiceWorker"
 
+func _get_export_options(platform: EditorExportPlatform) -> Array[Dictionary]:
+	return [
+		{
+			"option": {
+				"name": "include_coi_service_worker",
+				"type": TYPE_BOOL
+			},
+			"default_value": true
+		},
+		{
+			"option": {
+				"name": "generate_index_popout",
+				"type": TYPE_BOOL
+			},
+			"default_value": false
+		},
+		{
+			"option": {
+				"name": "popout_window_instead_of_tab",
+				"type": TYPE_BOOL
+			},
+			"default_value": false
+		}
+	]
+
+func _should_update_export_options(platform: EditorExportPlatform) -> bool:
+	if not platform is EditorExportPlatformWeb: return false
+	var u = update_export_options
+	update_export_options = false
+	return u
+
 func _export_begin(features: PackedStringArray, is_debug: bool, path: String, flags: int) -> void:
 	if features.has("web"):
-		exporting_web = true
+		if not has_method("get_option") or get_option("include_coi_service_worker"):
+			exporting_web = true
 		export_path = path
-	
+		if has_method("get_option") and get_option("generate_index_popout"):
+			if export_path.ends_with("index.html"):
+				push_error("ERROR: cannot export as index.html with generate_index_popout option set")
+			else:
+				var html = POPOUT_INDEX_HTML
+				if get_option("popout_window_instead_of_tab"):
+					var w = ProjectSettings.get_setting("display/window/size/window_width_override")
+					if w <= 0:
+						w = ProjectSettings.get_setting("display/window/size/viewport_width")
+					var h = ProjectSettings.get_setting("display/window/size/window_height_override")
+					if h <= 0:
+						h = ProjectSettings.get_setting("display/window/size/viewport_height")
+					html = html.replace("__PARAMS__", "onclick=\"window.open('__GAME_HTML__', '_blank', 'popup,innerWidth=" + str(w) + ",innerHeight=" + str(h) + "'); return false;\"")
+				else:
+					html = html.replace("__PARAMS__", "target=\"_blank\"")
+				html = html.replace("__GAME_HTML__", export_path.get_file())
+				html = html.replace("__TITLE__", ProjectSettings.get_setting("application/config/name"))
+				var file = FileAccess.open(export_path.get_base_dir().path_join("index.html"), FileAccess.WRITE)
+				file.store_string(html)
+				file.close()
+
 func _export_end() -> void:
 	if exporting_web:
 		var html := FileAccess.get_file_as_string(export_path)
@@ -35,4 +88,28 @@ if (!window.SharedArrayBuffer) {
 	document.getElementById('status').style.display = 'none';
 	setTimeout(() => document.getElementById('status').style.display = '', 1500);
 }
+"""
+
+const POPOUT_INDEX_HTML = """<doctype html>
+<html>
+<head>
+<title>__TITLE__</title>
+<style>
+body {
+	background-color: black;
+}
+div {
+	margin-top: 40vh;
+	text-align: center;
+}
+a {
+	font-size: 18pt;
+	color: #eaeaea;
+}
+</style>
+</head>
+<body>
+<div><a href="__GAME_HTML__" __PARAMS__>Play __TITLE__</a></div>
+</body>
+</html>
 """
